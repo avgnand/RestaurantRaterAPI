a42ba75858e3fe1910cebe4a75f42ea260c7f61a
Nick Andresen <nimandresen@gmail.com>
2022-07-05 11:57:37 -0400

    Progress through Canvas module 3.04

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -1,4 +1,7 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantRaterAPI.Models;
 
 namespace RestaurantRaterAPI.Controllers
 {
@@ -9,6 +12,35 @@ namespace RestaurantRaterAPI.Controllers
         private RestaurantDbContext _context;
         public RestaurantController(RestaurantDbContext context) {
             _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostRestaurant([FromForm] RestaurantEdit model) {
+            if (!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+            _context.Restaurants.Add(new Restaurant(){
+                Name = model.Name,
+                Location = model.Location,
+            });
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRestaurants() {
+            var restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetRestaurantById(int id) {
+            var restaurant = await _context.Restaurants.FindAsync(id);
+            if (restaurant == null){
+                return NotFound();
+            }
+            return Ok(restaurant);
         }
     }
 }
