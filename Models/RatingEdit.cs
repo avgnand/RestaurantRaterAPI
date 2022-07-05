99f0d12b76763fe15b643a7774a10125b74c87ee
Nick Andresen <nimandresen@gmail.com>
2022-07-05 15:59:39 -0400

    Progress through Canvas module 3.13

---
+++ Models/RatingEdit.cs
@@ -0,0 +1,13 @@
+
+
+namespace RestaurantRaterAPI.Models
+{
+    public class RatingEdit
+    {
+        public float FoodScore {get; set;}
+        public float CleanlinessScore {get; set;}
+        public float AtmosphereScore {get; set;}
+        public float Score {get; set;}
+        public int RestaurantId {get; set;}
+    }
+}
--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -14,5 +14,56 @@ namespace RestaurantRaterAPI.Models
         [Required]
         [MaxLength(100)]
         public string Location {get; set;}
+        
+        public virtual List<Rating> Ratings {get; set;} = new List<Rating>();
+
+        public double AverageFoodScore {
+            get {
+                if (Ratings.Count == 0) {
+                    return 0;
+                }
+                double total = 0.0;
+                foreach (var rating in Ratings) {
+                    total += rating.FoodScore;
+                }
+                return total / Ratings.Count;
+            }
+        }
+        public double AverageCleanlinessScore {
+            get {
+                if (Ratings.Count == 0) {
+                    return 0;
+                }
+                double total = 0.0;
+                foreach (var rating in Ratings) {
+                    total += rating.CleanlinessScore;
+                }
+                return total / Ratings.Count;
+            }
+        }
+        public double AverageAtmosphereScore {
+            get {
+                if (Ratings.Count == 0) {
+                    return 0;
+                }
+                double total = 0.0;
+                foreach (var rating in Ratings) {
+                    total += rating.AtmosphereScore;
+                }
+                return total / Ratings.Count;
+            }
+        }
+        public double AverageScore {
+            get {
+                if (Ratings.Count == 0) {
+                    return 0;
+                }
+                double total = 0.0;
+                foreach (var rating in Ratings) {
+                    total += rating.Score;
+                }
+                return total / Ratings.Count;
+            }
+        }
     }
 }
