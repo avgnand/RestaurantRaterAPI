a3a52bdab1e9b80dcca806349445dbdfd1f24519
Nick Andresen <nimandresen@gmail.com>
2022-07-05 11:33:18 -0400

    Progress through Canvas module 3.02

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _context;
+        public RestaurantController(RestaurantDbContext context) {
+            _context = context;
+        }
+    }
+}
